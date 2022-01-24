6a56a8ba7207a6930f97c7457f8b9fbd9ad92359
RSGM <30939186+rodgav@users.noreply.github.com>
2022-01-23 19:50:56 -0500

    build web 23-01-2022 2

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,1 +1,2 @@
-const String urlImageConst = 'http://192.168.0.107/api-subastalo/public/uploads/';
+const String urlImageConst = 'https://apisubastalo.rsgmsolutions.com/uploads/';
+//const String urlImageConst = 'http://192.168.0.107/api-subastalo/public/uploads/';
